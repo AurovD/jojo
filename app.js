6f018f64bbf5894418224bbb48c30be5480c25a0
AurovD <dmitry8891@mail.ru>
2021-01-29 11:15:52 +0300

    add db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let exp = require("express");
 let app = exp();
-let port = 3000;
+let port = 4000;
 app.use(exp.static("./public"));
+app.use("/api", require("./routes/test"));
 app.listen(port);
---
+++ routes/test.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: '194.58.121.72',
+    database: 'postgres',
+    port: 5432,
+});
+
+
+router.get("/test", (req,res) => {
+    pool.query('SELECT * FROM users', (err, result) => {
+        if (err) {
+            throw err;
+        } else if (result) {
+            res.send({
+                "msg": "ok"
+            });
+        }
+    });
+});
+
+module.exports = router;
