00f564790ea9d2e158259c99952d6ea14a9a8dd3
AurovD <dmitry8891@mail.ru>
2021-01-29 12:24:51 +0300

    user

--- routes/test.js
+++ routes/test.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'postgres',
+    user: 'dima',
     host: '194.58.121.72',
     database: 'postgres',
+    password: '123',
     port: 5432,
 });
 
 
 router.get("/test", (req,res) => {
+    console.log("test")
     pool.query('SELECT * FROM users', (err, result) => {
         if (err) {
             throw err;
